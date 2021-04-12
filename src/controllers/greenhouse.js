b3a3180903618c8f3722be7c70053565ec9c8a99
josemnr <maninari97@gmail.com>
2021-04-12 01:34:55 -0500

    Update controllers to get element by id

--- src/controllers/greenhouse.js
+++ src/controllers/greenhouse.js
@@ -1,8 +1,17 @@
 const greenhouse = require("../models/greenhouse");
+var ObjectId = require('mongodb').ObjectId;
 
 exports.getGreenhouses = (req, res) => {
     greenhouse.find({}, (err, results) => {
         console.log(results)
         res.send(results);
+    });
+}
+
+exports.getGreenhouse = (req, res) => {
+    console.log(req.params.id);
+    greenhouse.findOne({"_id":ObjectId(req.params.id)}, (err, results) => {
+        console.log(results)
+        res.send(results[0]);
     });
 }
--- src/controllers/seed.js
+++ src/controllers/seed.js
@@ -1,8 +1,17 @@
 const seed = require("../models/seed");
+var ObjectId = require('mongodb').ObjectId;
 
 exports.getSeeds = (req, res) => {
     seed.find({}, (err, results) => {
         console.log(results)
         res.send(results);
+    });
+}
+
+exports.getSeed = (req, res) => {
+    console.log(req.params.id);
+    seed.findOne({"_id":ObjectId(req.params.id)}, (err, results) => {
+        console.log(results)
+        res.send(results[0]);
     });
 }
