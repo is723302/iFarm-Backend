bf48b74d75454aaa20e9e5adabdf2fec5242a3db
josemnr <maninari97@gmail.com>
2021-04-12 01:33:35 -0500

    Add public endpoint

--- index.js
+++ index.js
@@ -14,15 +14,16 @@ const {
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use('/api/assets', express.static(path.join(__dirname, './public')));
 
 // Los routers se agregan a las rutas
-app.use('/', homePageRouter);
-app.use('/seeds', seedRouter);
-app.use('/messages', messageRouter);
-app.use('/calendar', calendarRouter);
-app.use('/employees', employeeRouter);
-app.use('/supervisors', supervisorRouter);
-app.use('/greenhouses', greenhouseRouter);
+app.use('/api', homePageRouter);
+app.use('/api/seeds', seedRouter);
+app.use('/api/messages', messageRouter);
+app.use('/api/calendar', calendarRouter);
+app.use('/api/employees', employeeRouter);
+app.use('/api/supervisors', supervisorRouter);
+app.use('/api/greenhouses', greenhouseRouter);
 
 app.listen(config.port, config.host, function() {
     console.log(`Listening on port ${config.port} and host ${config.host}`);
