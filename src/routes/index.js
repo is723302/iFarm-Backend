ae70e96406cd9c0484d9bae058ed595f58d34a84
josemnr <maninari97@gmail.com>
2021-04-19 07:56:37 -0500

    Update routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,29 @@
-const homePageRouter = require('./home_page');
-const seedRouter = require('./seed');
-const messageRouter = require('./message');
-const calendarRouter = require('./calendar');
-const employeeRouter = require('./employee');
-const supervisorRouter = require('./supervisor');
-const greenhouseRouter = require('./greenhouse');
+const express = require("express");
+const router = express.Router();
+// const homePageRouter = require('./home_page');
+// const seedRouter = require('./seed');
+// const messageRouter = require('./message');
+// const calendarRouter = require('./calendar');
+// const employeeRouter = require('./employee');
+// const supervisorRouter = require('./supervisor');
+// const greenhouseRouter = require('./greenhouse');
 
-module.exports = {
-    homePageRouter,
-    seedRouter,
-    messageRouter,
-    calendarRouter,
-    employeeRouter,
-    supervisorRouter,
-    greenhouseRouter
+const greenhouseApi = require('./greenhouse')
+
+function iFarmApi(app) {
+    greenhouseApi(app);
 }
+
+
+
+// module.exports = {
+//     homePageRouter,
+//     seedRouter,
+//     messageRouter,
+//     calendarRouter,
+//     employeeRouter,
+//     supervisorRouter,
+//     greenhouseRouter
+// }
+
+module.exports = iFarmApi;
