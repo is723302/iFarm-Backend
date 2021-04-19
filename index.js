4013ed1cd61bd6f4574d25d3b17f34ea773cd236
josemnr <maninari97@gmail.com>
2021-04-19 07:57:19 -0500

    Add validations and error handlers middlewares

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
 const express = require('express');
-const path = require('path');
 const app = express();
-const {config} = require('./config');
+const path = require('path');
+const { config } = require('./config');
+const iFarmApi = require('./src/routes/index');
 const {
-    homePageRouter,
-    seedRouter,
-    messageRouter,
-    calendarRouter,
-    employeeRouter,
-    supervisorRouter,
-    greenhouseRouter
-} = require('./src/routes');
+    logErrors,
+    wrapErrors,
+    errorHandler 
+} = require('./src/utils/middleware/errorHandlers')
+const notFoundHandler = require('./src/utils/middleware/notFoundHandler');
 
 app.use(express.urlencoded({extended: false}));
-app.use(express.json());
 app.use('/api/assets', express.static(path.join(__dirname, './public')));
 
-// Los routers se agregan a las rutas
-app.use('/api', homePageRouter);
-app.use('/api/seeds', seedRouter);
-app.use('/api/messages', messageRouter);
-app.use('/api/calendar', calendarRouter);
-app.use('/api/employees', employeeRouter);
-app.use('/api/supervisors', supervisorRouter);
-app.use('/api/greenhouses', greenhouseRouter);
+// body parser
+app.use(express.json());
+
+// routes
+iFarmApi(app);
+
+// Catch 404
+app.use(notFoundHandler);
+
+// Errors middleware
+app.use(logErrors);
+app.use(wrapErrors);
+app.use(errorHandler);
 
 app.listen(config.port, config.host, function() {
-    console.log(`Listening on port ${config.port} and host ${config.host}`);
+    console.log(`Listening http://${config.host}:${config.port}`);
 });
