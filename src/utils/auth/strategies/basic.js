b89fc7efbf6087ad331612155b48f4ae148a78b0
josemnr <maninari97@gmail.com>
2021-04-28 18:36:59 -0500

    Update basic strategie

--- src/utils/auth/strategies/basic.js
+++ src/utils/auth/strategies/basic.js
@@ -1,5 +1,22 @@
+const bcrypt = require('bcrypt');
+const boom = require('@hapi/boom');
 const passport = require('passport');
 const { BasicStrategy } = require('passport-http');
-const boom = require('@hapi/boom');
+const UserController = require('./../../../controllers/user');
 
-const UserController = require('./../../../controllers/user')
+passport.use(new BasicStrategy(async function(email, password, cb) {
+    const userController = new UserController();
+    try {
+        const user = await userController.getUserByEmail({ email });
+        if(!user) {
+            return cb(boom.unauthorized(), false);
+        }
+        if(!(await bcrypt.compare(password, user.password))) {
+            return cb(boom.unauthorized(), false);
+        }
+        delete user.password;
+        return cb(null, user);
+    } catch (error) {
+        return cb(error);
+    }
+}));
