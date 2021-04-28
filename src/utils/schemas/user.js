f4dac7e2bce935e13c1c64bd5ff1ab658395bb33
josemnr <maninari97@gmail.com>
2021-04-28 18:40:51 -0500

    Update user schema

--- src/utils/schemas/user.js
+++ src/utils/schemas/user.js
@@ -5,6 +5,7 @@ const userEmailSchema = joi.string().email();
 const userNameSchema = joi.string().max(60);
 const userPasswordSchema = joi.string();
 const userRoleSchema = joi.string();
+const userGoogleIdSchema = joi.string();
 const userProfilePicSchema = joi.string();
 const supervisorIdSchema = joi.string().regex(/^[0-9a-fA-F]{24}$/);
 const greenhousesIdSchema = joi.array();
@@ -13,10 +14,11 @@ const createUserSchema = {
     email: userEmailSchema.required(),
     name: userNameSchema.required(),
     password: userPasswordSchema.required(),
-    role: userRoleSchema.required(),
-    profile_pic: userProfilePicSchema.required(),
+    role: userRoleSchema,
+    google_id: userGoogleIdSchema,
+    profile_pic: userProfilePicSchema,
     supervisor_id: supervisorIdSchema,
-    greenhouses_id: greenhousesIdSchema.required(),
+    greenhouses_id: greenhousesIdSchema,
 };
 
 const updateUserSchema = {
